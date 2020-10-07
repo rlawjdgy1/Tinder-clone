ee1e3bc2992a73e8a3ed1ca0f3fe4e7eebb35b0c
Junghyo <junghyo@Junghyoui-MacBookPro.local>
2020-10-08 02:55:32 +0900

    second commit

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import TinderCard from "react-tinder-card";
+import './TinderCards.css';
 
 function TinderCards() {
     const [people, setPeople] = useState([
         {
             name: 'steve jobs',
-            url: "https://upload.wikimedia.org/wikipedia/commons/d/dc/Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg"
+            url: "https://vignette.wikia.nocookie.net/disney/images/5/54/Steve_Jobs.jpg/revision/latest?cb=20190223220738"
         },
         {
             name: 'mark zuckerberg',
-            url: "https://en.wikipedia.org/wiki/Mark_Zuckerberg#/media/File:Mark_Zuckerberg_F8_2019_Keynote_(32830578717)_(cropped).jpg"
+            url: "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5c76b7d331358e35dd2773a9%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D4401%26cropY1%3D0%26cropY2%3D4401"
         }
     ]);
     // const people = [];
